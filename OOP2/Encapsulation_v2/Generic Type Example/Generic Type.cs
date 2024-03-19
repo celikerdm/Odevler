aecea3966d322ee2e93d45e70d9f1c0c2cb1f28b
celikerdm <162330914+celikerdm@users.noreply.github.com>
2024-03-19 16:37:42 +0300

    Add files via upload

---
+++ OOP2/Encapsulation_v2/Generic Type Example/Generic Type.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_v2
+{
+    internal class Generic_Type
+    {
+        public class Student<T>
+        {
+            
+            public T data;
+
+            
+            public Student(T data)
+            {
+                this.data = data;
+                Console.WriteLine("Data passed: " + this.data);
+            }
+        }
+
+        class Program
+        {
+            
+            
+               
+                Student<string> studentName = new Student<string>("Ahmet");
+
+                
+                Student<int> studentId = new Student<int>(20);
+            
+        }
+    }
+}
---
+++ OOP2/Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Polymorphism.Example1;
+using Polymorphism.Example2;
+
+
+ILoan farmerLoan = new FarmerLoan();
+ILoan businessLoan = new BusinessLoan();
+
+
+farmerLoan.Calculate();
+businessLoan.Calculate();
+
+
+Console.WriteLine("---------------------------------");
+
+
+
+Ticket sports = new Sports();
+sports.GetTicket();
+Ticket movie = new Movie();
+movie.GetTicket();
+
+
+
+Console.WriteLine("---------------------------------");
+
