5669b95d14b8eceb95454f740853985508f866f6
celikerdm <162330914+celikerdm@users.noreply.github.com>
2024-03-19 16:47:04 +0300

    UpdateGenericType.cs

--- OOP2/Encapsulation_v2/Generic Type Example/Generic Type.cs
+++ OOP2/Encapsulation_v2/Generic Type Example/Generic Type.cs
@@ -6,31 +6,27 @@ using System.Threading.Tasks;
 
 namespace Encapsulation_v2
 {
-    internal class Generic_Type
+    public class Student<T>
+{
+     
+    public T data;
+
+   
+    public Student(T data)
     {
-        public class Student<T>
-        {
-            
-            public T data;
-
-            
-            public Student(T data)
-            {
-                this.data = data;
-                Console.WriteLine("Data passed: " + this.data);
-            }
-        }
-
-        class Program
-        {
-            
-            
-               
-                Student<string> studentName = new Student<string>("Ahmet");
-
-                
-                Student<int> studentId = new Student<int>(20);
-            
-        }
+        this.data = data;
+        Console.WriteLine(this.data);
     }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Student<string> studentName = new Student<string>("Ahmet");
+
+        Student<int> studentId = new Student<int>(10);
+    }
+}
 }
